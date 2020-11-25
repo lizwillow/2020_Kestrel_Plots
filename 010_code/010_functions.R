cfbd356ccd55379773c6d33d93296eb2a2597b6f
Elizabeth Eisenhauer <liz.w.eisenhauer@gmail.com>
2020-11-24 20:17:33 -0500

    update PA plots

--- 010_code/010_functions.R
+++ 010_code/010_functions.R
@@ -25,29 +25,65 @@ kestrel_plot_cumulative <- function(df, region) {
 }
 
 # total num chicks by org
-kestrel_plot_chicks_per_year <- function(df, region, combined = FALSE) {
+kestrel_plot_chicks_per_year <- function(df, region, combined = FALSE, labels_as_points = FALSE, 
+                                         label_col = chicks_banded, cols) {
+  if (missing(cols)) {
+    cols = gg_color(length(unique(df$org)))
+  }
+  label_col = enquo(label_col)
   if (combined == FALSE) {
-    df %>% ggplot() +
-      theme_minimal() +
-      geom_line(aes(x=year, y=chicks_banded, col=org),
-                alpha=0.6 , size=.5) +
-      geom_point(aes(x = year, y = chicks_banded, col=org), size = 3) +
-      # scale_color_viridis(discrete = T, end = 0.9) +
-      ggtitle(paste(region, "kestrel nest box programs")) +
-      labs(subtitle = "Number of banding-age kestrel chicks per year") +
-      scale_x_date(date_breaks = "1 year", date_labels = "%Y",
-                   minor_breaks = NULL) +
-      theme(axis.text.x=element_text(angle=60, hjust=1),
-            axis.title = element_blank(),
-            legend.title = element_blank(),
-            legend.position = "bottom",
-            ## center titles and make main title bold:
-            plot.title = element_text(hjust = 0.5, face="bold"),
-            plot.subtitle = element_text(hjust = 0.5),
-            text = element_text(size=14)) +
-      ggrepel::geom_text_repel(aes(x = year, y = chicks_banded,
-                                   label=chicks_banded),
+    if (labels_as_points == TRUE) {
+      df %>% ggplot() +
+        theme_minimal() +
+        geom_line(aes(x=year, y=chicks_banded, col=org),
+                  alpha=0.6 , size=.5) +
+        # geom_point(aes(x = year, y = chicks_banded, col=org), size = 3) +
+        geom_label(aes(x = year, y = chicks_banded, col=org,
+                       label = !!label_col), size = 3) +
+        scale_color_manual(values = cols) +
+        # scale_color_viridis(discrete = T, end = 0.9) +
+        ggtitle(paste(region, "kestrel nest box programs")) +
+        labs(subtitle = "Number of banding-age kestrel chicks per year") +
+        scale_x_date(date_breaks = "1 year", date_labels = "%Y",
+                     minor_breaks = NULL) +
+        theme(axis.text.x=element_text(angle=60, hjust=1),
+              axis.title = element_blank(),
+              legend.title = element_blank(),
+              legend.position = "bottom",
+              ## center titles and make main title bold:
+              plot.title = element_text(hjust = 0.5, face="bold"),
+              plot.subtitle = element_text(hjust = 0.5),
+              text = element_text(size=14))
+      # ggrepel::geom_text_repel(aes(x = year, y = chicks_banded,
+      #                              label=chicks_banded),
+      #                          hjust=.5, vjust=2, size = 4)
+    } else {
+      df %>% ggplot() +
+        theme_minimal() +
+        geom_line(aes(x=year, y=chicks_banded, col=org),
+                  alpha=0.6 , size=.5) +
+        geom_point(aes(x = year, y = chicks_banded, col=org), size = 3) +
+        # geom_label(aes(x = year, y = chicks_banded, col=org,
+        #                label = chicks_banded), size = 3) +
+        scale_color_manual(values = cols) +
+        # scale_color_viridis(discrete = T, end = 0.9) +
+        ggtitle(paste(region, "kestrel nest box programs")) +
+        labs(subtitle = "Number of banding-age kestrel chicks per year") +
+        scale_x_date(date_breaks = "1 year", date_labels = "%Y",
+                     minor_breaks = NULL) +
+        theme(axis.text.x=element_text(angle=60, hjust=1),
+              axis.title = element_blank(),
+              legend.title = element_blank(),
+              legend.position = "bottom",
+              ## center titles and make main title bold:
+              plot.title = element_text(hjust = 0.5, face="bold"),
+              plot.subtitle = element_text(hjust = 0.5),
+              text = element_text(size=14)) +
+        ggrepel::geom_text_repel(aes(x = year, y = chicks_banded,
+                                   label=!!label_col),
                                hjust=.5, vjust=2, size = 4)
+    }
+    
   } else {
     df %>% ggplot() +
       geom_line(aes(x = year, y = sum_chicks_banded_per_year), color="#69b3a2", alpha = 0.5,
@@ -97,6 +133,5 @@ kestrel_plot_chicks_per_box <- function(df, region, text_repel_size = 4) {
     ggtitle(paste(region, "kestrel nest box programs")) +
     labs(subtitle = "Average number of banding-age chicks per nested box (failures are entered as zeroes)") +
     scale_x_date(date_breaks = "1 year", date_labels = "%Y",
-                 minor_breaks = NULL) +
-    scale_y_continuous(limits = c(0, NA))
+                 minor_breaks = NULL)
 }
